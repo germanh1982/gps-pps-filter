3b38364fbc7bd97d99df2d6062eec0f102a78857
Germán Herrera <germanh1982@gmail.com>
2023-12-03 15:27:34 -0300

    chore: improvements.

--- kalman.py
+++ kalman.py
@@ -1,20 +1,23 @@
 import numpy as np
-import scipy
 from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import KalmanFilter
 
 class Kalman:
-    def __init__(self, initial_x, initial_v):
-        self._kalman = KalmanFilter (dim_x=2, dim_z=1)
+    def __init__(self, initial_x, initial_v, meas_noise, proc_noise_var):
+        self._kalman = KalmanFilter(dim_x=2, dim_z=1)
         self._kalman.x = np.array([initial_x, initial_v])    # initial values: position, velocity
         self._kalman.F = np.array([[1.,1.], [0.,1.]]) # state transition matrix
         self._kalman.H = np.array([[1.,0.]]) # measurement function
         self._kalman.P = np.array([[1000., 0.], [0., 1000.]]) # covariance matrix
-        self._kalman.R = np.array([[10000.]]) # measurement noise
-        self._kalman.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.013) # process noise
+        self._kalman.R = np.array([[meas_noise]]) # measurement noise
+        self._kalman.Q = Q_discrete_white_noise(dim=2, dt=1, var=proc_noise_var) # process noise
 
     def feed(self, value):
         self._kalman.predict()
         self._kalman.update(value)
+        return self._kalman.x
+
+    @property
+    def state(self):
         return self._kalman.x
 
--- main.py
+++ main.py
@@ -1,54 +1,107 @@
+import queue
+from paho.mqtt.client import Client
+import logging
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from argparse import ArgumentParser
 import numpy as np
-from mqttclient import MQTTClient
 from circbuf import CircularBuffer
 from pfconv import PhaseFreqConv
+from kalman import Kalman
+
+def initialize_mqtt(qm):
+    # set up mqtt consumer
+    def on_message(client, userdata, msg):
+        qm.put((msg.topic, msg.payload))
+
+    client = Client()
+    client.connect(args.host)
+    client.on_message = on_message
+    client.loop_start()
+    client.subscribe([
+        (f'meas/{args.channel}', 0),
+        ('qerr', 0)
+    ])
 
 def main():
+    # set up data processing blocks
     phase_diff = PhaseFreqConv(args.period)
     phase_diff_corr = PhaseFreqConv(args.period)
-    buf = CircularBuffer(args.bufsize, 3)
-    mqtt = MQTTClient(host=args.host, topics=['meas/chA', 'qerr'])
+    buf = CircularBuffer(args.bufsize, 2)
+    filt = Kalman(args.startx, args.startv, meas_noise=args.measnoise, proc_noise_var=args.procnoise)
 
-    # enable interactive mode
-    plt.ion()
+    qerr = [0] # PPS quantization error: keep value in mutable type to avoid nonlocal declaration in drawing callback()
 
-    qerr = 0
+    # configure plot
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
 
-    while True:
-        for topic, value in mqtt.get():
+    # queue to synchronize data from mqtt into drawing callback
+    qm = queue.Queue()
+
+    initialize_mqtt(qm)
+
+    # line labels
+    labels = [
+        'freq_corr',
+        'filt_freq',
+    ]
+
+    def animate(frame):
+        # drawing callback
+
+        while True:
+            try:
+                topic, value = qm.get_nowait()
+
+            except queue.Empty:
+                break
+
             if topic == 'qerr':
-                qerr = float(value) * 1e-12 # convert from ps to s
-                print(f"qerr = {qerr}")
+                # value is phase quantization error
+                qerr[0] = float(value) * 1e-12 # convert from ps to s
 
-            elif topic == 'meas/chA':
+            elif topic == f'meas/{args.channel}':
+                # value is PPS phase
                 phase = float(value)
                 freq = phase_diff.feed(phase)
-                freq_corr = phase_diff_corr.feed(phase + qerr)
+                freq_corr = phase_diff_corr.feed(phase + qerr[0])
 
-                print(f"freq={freq} freq_corr={freq_corr}")
+                if freq_corr is not None:
+                    filt_freq, filt_drift = filt.feed(freq_corr)
+                    log.debug(f"freq={freq_corr} qerr={qerr[0]} filt_freq={filt_freq}")
 
-                if freq is not None:
-                    # frequency lowpass filtering
-                    #out1 = np.array(filt1.feed(freq))
-
+                if freq is not None and filt_freq is not None:
                     # append sample to circular buffer
-                    buf.append(freq, qerr, freq_corr)
+                    buf.append(freq_corr, filt_freq)
 
                     # plot results
-                    plt.plot(buf.get(), label=['freq', 'qerr', 'corrected'])
-                    plt.grid()
-                    plt.legend()
-                    plt.draw()
-                    plt.pause(1)
-                    plt.clf()
+                    ax1.clear()
+                    ax1.plot(buf.get(), label=labels)
+                    ax1.grid()
+                    ax1.legend()
+
+    ani = FuncAnimation(fig, animate, interval=args.period * 1000, cache_frame_data=False)
+
+    try:
+        plt.show()
+    except KeyboardInterrupt:
+        print(f"Last state: --startx={filt.state[0]} --startv={filt.state[1]}")
 
 if __name__ == '__main__':
     p = ArgumentParser()
     p.add_argument('--bufsize', type=int, default=500, help='display buffer length')
     p.add_argument('--period', type=int, default=1, help='sample period')
+    p.add_argument('--measnoise', type=float, default=1e4, help='Measurement noise')
+    p.add_argument('--procnoise', type=float, default=0, help='Process noise')
+    p.add_argument('--channel', choices=['chA', 'chB', 'chC', 'chD'], default='chA', help='TICC sampling channel')
+    p.add_argument('--startx', type=float, default=0, help="Initial filter state for x")
+    p.add_argument('--startv', type=float, default=0, help="Initial filter state for v")
     p.add_argument('host')
     args = p.parse_args()
+
+    logging.basicConfig()
+    log = logging.getLogger('gpsfilter')
+    log.setLevel(logging.DEBUG)
+
     main()
